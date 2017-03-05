01d56e6186ae257d8f1bb57c382905cf303da4f1
bear2107 <Piyush.joshi@iiitb.org>
2017-03-05 13:28:37 +0530

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -2,10 +2,7 @@ module.exports=function(app)
 {
 	var homeRoute=App.route('homeroute')
 	app.get("/",homeRoute.home)
-	var getstate=App.route('getstate');
-	//app.get("/state/:var",getstate.state);
-	var transaction=App.route('transact')
-	app.get('/transaction',transaction.trans);
+	
 
 }
 
