80c7fd8a8594129c09854ee0d9e27bfee7ddf664
bear2107 <Piyush.joshi@iiitb.org>
2017-03-06 23:20:54 +0530

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,33 @@
+
 $(document).ready(function(){
 
+
+
 	$('#reader').html5_qrcode(function(data){
-			$('#read').html(data);
-			windows.location='/transaction'
+		//alert("piyush");
+		$('#read').html(data);
+		var amount=data;
+		console.log(amount);
+		//alert("joshi");
+			$.ajax({
+				    type: "post",
+				    url: '/transactions',
+					data: { amount: data},
+				    datatype: "json",
+				    success: function(result) {
+				       console.log("Status: " + result.status);
+				    }
+				});
+
+		//	window.location='/ass'
+			//console.log(x+"hgf");
 		},
+
 		function(error){
 			$('#read_error').html(error);
 		}, function(videoError){
 			$('#vid_error').html(videoError);
 		}
+
 	);
 });
